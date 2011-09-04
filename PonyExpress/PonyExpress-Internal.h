05b6ee3898388fb47c1ad76b2ee1e3861818d71d
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-09-04 11:39:27 -0700

    add shared internal loggers

--- PonyExpress/PonyExpress-Internal.h
+++ PonyExpress/PonyExpress-Internal.h
@@ -5,3 +5,17 @@
 //  Created by Jean-Pierre Mouilleseaux on 4 Sept 2011.
 //  Copyright (c) 2011 Chorded Constructions. All rights reserved.
 //
+
+#ifdef DEBUG
+    #define CCDebugLogSelector() NSLog(@"-[%@ %@]", /*NSStringFromClass([self class])*/self, NSStringFromSelector(_cmd))
+    #define CCDebugLog(a...) NSLog(a)
+    #define CCWarningLog(a...) NSLog(a)
+    #define CCErrorLog(a...) NSLog(a)
+#else
+    #define CCDebugLogSelector()
+    #define CCDebugLog(a...)
+    #define CCWarningLog(a...) NSLog(a)
+    #define CCErrorLog(a...) NSLog(a)
+#endif
+
+#define CCLocalizedString(key, comment) [[NSBundle bundleForClass:[self class]] localizedStringForKey:(key) value:@"" table:(nil)]
