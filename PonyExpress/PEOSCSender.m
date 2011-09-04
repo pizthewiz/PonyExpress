4a2d729aa19b5a7d4504bf55df83ec8b8a3a4c4b
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-09-04 11:44:07 -0700

    add simple means to build up and tear down UDP socket

--- PonyExpress/PEOSCSender.m
+++ PonyExpress/PEOSCSender.m
@@ -7,10 +7,18 @@
 //
 
 #import "PEOSCSender.h"
+#import "PonyExpress-Internal.h"
+#import "AsyncUdpSocket.h"
+
+@interface PEOSCSender()
+@property (nonatomic, retain) AsyncUdpSocket* socket;
+- (BOOL)_setupSocket;
+- (BOOL)_tearDownSocket;
+@end
 
 @implementation PEOSCSender
 
-@synthesize host, port;
+@synthesize socket, host, port;
 
 + (id)senderWithHost:(NSString*)host port:(UInt16)port {
     PEOSCSender* sender = [[PEOSCSender alloc] initWithHost:host port:port];
@@ -22,14 +30,45 @@
     if (self) {
         self.host = hos;
         self.port = por;
+
+        [self _setupSocket];
     }
     return self;
+}
+
+- (void)dealloc {
+    [self _tearDownSocket];
+
+    [super dealloc];
 }
 
 #pragma mark -
 
 - (NSString*)description {
     return [NSString stringWithFormat:@"<%@: %@:%d>", NSStringFromClass([self class]), self.host, self.port];
+}
+
+#pragma mark - PRIVATE
+ 
+ - (BOOL)_setupSocket {
+     AsyncUdpSocket* soc = [[AsyncUdpSocket alloc] init];
+     self.socket = soc;
+     [soc release];
+
+     NSError* error = nil;
+     BOOL status = [self.socket bindToAddress:self.host port:self.port error:&error];
+     if (!status) {
+         CCErrorLog(@"ERROR - failed to bind go address: %@:%d - %@", self.host, self.port, [error localizedDescription]);
+         return NO;
+     }
+
+     return YES;
+}
+
+- (BOOL)_tearDownSocket {
+    [self.socket close];
+
+    return YES;
 }
 
 @end
