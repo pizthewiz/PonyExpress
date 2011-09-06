99e5f3a4e2ed0a437bf15936268f2466c1ac5021
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-09-06 10:00:03 -0700

    denote blob type as untested

--- PonyExpress/PEOSCMessage.h
+++ PonyExpress/PEOSCMessage.h
@@ -22,7 +22,7 @@
 extern NSString* const PEOSCMessageTypeTagInteger; // NSNumber
 extern NSString* const PEOSCMessageTypeTagFloat; // NSNumber
 extern NSString* const PEOSCMessageTypeTagString; // NSString (ASCII)
-extern NSString* const PEOSCMessageTypeTagBlob; // NSData - UNSUPPORTED
+extern NSString* const PEOSCMessageTypeTagBlob; // NSData - UNTESTED
 extern NSString* const PEOSCMessageTypeTagTrue;
 extern NSString* const PEOSCMessageTypeTagFalse;
 extern NSString* const PEOSCMessageTypeTagNull;
