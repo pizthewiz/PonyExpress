fac5d5d8c569f53c446a3ba017140de79a835394
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-09-07 12:27:07 -0700

    split out message tests

---
+++ PonyExpressTests/PonyExpressMessageTests.h
@@ -0,0 +1,12 @@
+//
+//  PonyExpressMessageTests.h
+//  PonyExpress
+//
+//  Created by Jean-Pierre Mouilleseaux on 7 Sept 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PonyExpressMessageTests : SenTestCase
+@end
---
+++ PonyExpressTests/PonyExpressMessageTests.m
@@ -0,0 +1,12 @@
+//
+//  PonyExpressMessageTests.m
+//  PonyExpress
+//
+//  Created by Jean-Pierre Mouilleseaux on 7 Sept 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import "PonyExpressMessageTests.h"
+
+@implementation PonyExpressMessageTests
+@end
