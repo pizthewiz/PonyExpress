9634403bd05babec4aa214218fdddb0a7a71efbd
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-09-30 15:32:42 -0700

    add placeholder for receiver tests

---
+++ PonyExpressTests/PonyEpressReceiverTests.h
@@ -0,0 +1,12 @@
+//
+//  PonyEpressReceiverTests.h
+//  PonyExpress
+//
+//  Created by Jean-Pierre Mouilleseaux on 30 Sept 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface PonyEpressReceiverTests : SenTestCase
+@end
---
+++ PonyExpressTests/PonyEpressReceiverTests.m
@@ -0,0 +1,12 @@
+//
+//  PonyEpressReceiverTests.m
+//  PonyExpress
+//
+//  Created by Jean-Pierre Mouilleseaux on 30 Sept 2011.
+//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//
+
+#import "PonyEpressReceiverTests.h"
+
+@implementation PonyEpressReceiverTests
+@end
