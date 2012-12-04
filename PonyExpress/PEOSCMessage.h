5da244f7d499a8e525bd2e9ed2f97b2bbf3c50e3
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2012-12-03 17:48:18 -0800

    fix copyright date, this file was modified

--- PonyExpress/PEOSCMessage.h
+++ PonyExpress/PEOSCMessage.h
@@ -3,7 +3,7 @@
 //  PonyExpress
 //
 //  Created by Jean-Pierre Mouilleseaux on 2 Sept 2011.
-//  Copyright (c) 2011 Chorded Constructions. All rights reserved.
+//  Copyright (c) 2011-2012 Chorded Constructions. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
