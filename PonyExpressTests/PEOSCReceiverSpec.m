940f2b1c113dd77d1c383edf77c03b0103b724d8
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2013-02-21 23:00:04 -0800

    add stubs for sender and receiver specs

---
+++ PonyExpressTests/PEOSCReceiverSpec.m
@@ -0,0 +1,10 @@
+//
+//  PEOSCReceiverSpec.m
+//  PonyExpress
+//
+//  Created by Jean-Pierre Mouilleseaux on 21 Feb 2013.
+//  Copyright (c) 2013 Chorded Constructions. All rights reserved.
+//
+
+SpecBegin(PEOSCReceiverSpec)
+SpecEnd
